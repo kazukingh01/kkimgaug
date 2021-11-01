d4a715b4ff25988ce4b17324f9843c04dc99fd1a
kazunoki <kazukingh01@gmail.com>
2021-11-01 16:02:30 +0900

    modify

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='kkimgaug',
-    version='2.0.0',
+    version='2.0.1',
     description='augmentation wrapper package for albumentations',
     long_description=long_description,
     long_description_content_type="text/markdown",
